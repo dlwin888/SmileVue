c6acd031ded46efdf1e38c6d7cbb00713cc02444
dlwin888 <duanle888@gmail.com>
2018-05-29 17:59:22 +0800

    引入Vant组件

---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{msg}}
+        <van-row>
+            <van-col span="8">span: 8</van-col>
+            <van-col span="8">span: 8</van-col>
+            <van-col span="8">span: 8</van-col>
+        </van-row>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Shoping Mall"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
